7e22c1bb37a8a708ffcb49351c8ba2faa2269a77
Jusung Hwang(nabigraphics) <hbh2692@naver.com>
2021-08-15 05:38:38 +0900

    ui: Add loading

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ import GlobalStyle from "./components/GlobalStyle";
 import styled from "@emotion/styled";
 
 const App = () => {
-  const [initialized, setInitialized] = useState(false);
   const [file, setFile] = useState<File | null>(null);
+  const [progress, setProgress] = useState(0);
 
   const { isLoading, isDone, isError, convertedData, convert } =
     useFFmpegConvert(file);
@@ -29,6 +29,16 @@ const App = () => {
   useEffect(() => {
     FFmpegService.init();
   }, []);
+
+  useEffect(() => {
+    if (isLoading) {
+      FFmpegService.ffmpeg.setProgress(({ ratio }) => {
+        setProgress(ratio);
+      });
+    } else {
+      setProgress(0);
+    }
+  }, [isLoading]);
 
   return (
     <>
@@ -47,7 +57,21 @@ const App = () => {
           />
         )}
         {/* Loading Component */}
-        {isLoading && <div>Loading...</div>}
+        {isLoading && (
+          <StyledLoadingContainer>
+            <StyledProgressBarWrapper>
+              <StyledProgressBar
+                style={{
+                  width: Intl.NumberFormat("en", { style: "percent" }).format(
+                    progress
+                  ),
+                }}
+              />
+            </StyledProgressBarWrapper>
+            Converting...{" "}
+            {Intl.NumberFormat("en", { style: "percent" }).format(progress)}
+          </StyledLoadingContainer>
+        )}
         {/* Result Component */}
         {file && isDone && (
           <ConvertResultContainer
@@ -69,4 +93,31 @@ const StyledLayoutContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const StyledLoadingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 16px;
+  text-align: center;
+  color: #232c30;
+`;
+
+const StyledProgressBarWrapper = styled.div`
+  width: 140px;
+  height: 4px;
+  position: relative;
+  z-index: 10;
+  background-color: #e5f0f3;
+  margin-bottom: 8px;
+`;
+
+const StyledProgressBar = styled.div`
+  width: 100%;
+  height: 4px;
+  background-color: #ab4b86;
 `;
