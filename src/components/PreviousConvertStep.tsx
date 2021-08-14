2effaeeb73664855a3429cdc31149f59348fc6f0
Jusung Hwang(nabigraphics) <hbh2692@naver.com>
2021-08-15 04:42:43 +0900

    ui: Add style of PreviousConvertStep

--- src/components/PreviousConvertStep.tsx
+++ src/components/PreviousConvertStep.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import React, { useEffect, useMemo, useState } from "react";
 import useBlobUrl from "../hooks/useBlobUrl";
 import getFilenameWithoutFileType from "../utils/getFilenameWithoutFileType";
@@ -29,10 +30,50 @@ const PreviousConvertStep = ({
       {blobUrl && (
         <FileCard src={blobUrl} fileName={fileName} fileType={fileType} />
       )}
-      <button onClick={onReset}>Reset</button>
-      <button onClick={onConvert}>Convert</button>
+      <StyledButtonContainer>
+        <StyledButton onClick={onReset}>Reset</StyledButton>
+        <StyledConvertButton onClick={onConvert}>
+          Convert File
+        </StyledConvertButton>
+      </StyledButtonContainer>
     </div>
   );
 };
 
 export default PreviousConvertStep;
+
+const StyledButton = styled.button`
+  appearance: none;
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 16px 24px;
+  height: 56px;
+
+  border: none;
+  background-color: #e5f0f3;
+  box-sizing: border-box;
+  border-radius: 4px;
+
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 24px;
+  text-align: center;
+  color: #232c30;
+`;
+
+const StyledConvertButton = styled(StyledButton)`
+  margin-left: 12px;
+  background-color: #96306e;
+  color: #fff;
+`;
+
+const StyledButtonContainer = styled.div`
+  width: 100%;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+`;
